37622518422385b6b120a8d1fb16cfd6e4a0b5d8
Mark Hamstra <hello@markhamstra.com>
2013-08-13 15:06:02 +0200

    Fix license comments

--- core/components/simpleab/model/simpleab/sabpick.class.php
+++ core/components/simpleab/model/simpleab/sabpick.class.php
@@ -2,23 +2,11 @@
 /**
  * SimpleAB
  *
- * Copyright 2011-2013 by Mark Hamstra <mark@modx.com>
+ * Copyright 2011-2013 by Mark Hamstra <support@modmore.com>
  *
- * This file is part of SimpleAB
+ * This file is part of SimpleAB.
  *
- * SimpleAB is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * SimpleAB is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * SimpleAB; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA 02111-1307 USA
- *
+ * For license terms, please review core/components/simpleab/docs/license.txt.
 */
 class sabPick extends xPDOSimpleObject {
 
